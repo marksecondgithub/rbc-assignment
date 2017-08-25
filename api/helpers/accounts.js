178b286423bf1e85d4c2b51f0f9581ba39c417c1
Mark Jamieson <mark@raisedmedia.com>
2017-08-25 12:21:01 -0300

    Add sparse property to Account.number

--- api/helpers/accounts.js
+++ api/helpers/accounts.js
@@ -3,15 +3,13 @@ let mongoose = require('mongoose')
 const AccountSchema = mongoose.Schema({
   number: {
     type: Number,
-    unique: true
+    unique: true,
+    sparse: true
   },
   type: String,
   status: String
 })
 
-const Account = mongoose.model('Account', AccountSchema);
-
 module.exports = {
-  AccountSchema,
-  Account
+  AccountSchema
 }
