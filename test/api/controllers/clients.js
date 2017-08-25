261ab1a9001a794d8fd0d968dd28b8882db02637
Mark Jamieson <mark@raisedmedia.com>
2017-08-25 12:21:47 -0300

    Update tests

--- test/api/controllers/clients.js
+++ test/api/controllers/clients.js
@@ -1,35 +1,55 @@
 var should = require('should');
 var request = require('supertest');
 var server = require('../../../app');
+var db = require('../../../api/helpers/db')
+var Client = require('../../../api/helpers/clients').Client
 
 let MongoClient = require('mongodb').MongoClient
 
 describe('controllers', function() {
   describe('clients', function() {
-    describe('GET /clients', function() {
-      it('should return an empty list', function(done) {
-        clearTestDB().then(() => {
-          request(server)
-            .get('/clients')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .end(function(err, res) {
-              should.not.exist(err);
-              res.body.should.eql([]);
-              done();
-            });
-        })
-      });
-    });
-
-    describe('POST /clients', function() {
+    let client1 = null
+    beforeEach(function(done) {
       let clientObj = {
         name: "Mark",
         address: "123 Fake St",
         postalCode: "A1A 1A1",
         phone: "222 555 8888",
         email: "mark@gmail.com",
+        dob: "1980-01-01"
+      }
+      db.insertClient(clientObj).then(client => {
+        client1 = client
+        done()
+      })
+    })
+    afterEach(function(done) {
+      Client.remove({}).then(() => {done()})
+    })
+    describe('GET /clients', function() {
+      it('should return a list of 1 item', function(done) {
+        request(server)
+          .get('/clients')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+            should.not.exist(err);
+            Client.find().then(function(clients){
+              should(clients.length).eql(1)
+              done()
+            })
+          });
+      });
+    });
+
+    describe('POST /clients', function() {
+      let clientObj = {
+        name: "Mark 2",
+        address: "234 Fake St",
+        postalCode: "B1A 1A1",
+        phone: "333 555 8888",
+        email: "mark2@gmail.com",
         dob: "1980-01-01"
       }
       it('should add a record to clients collection', function(done) {
@@ -44,33 +64,74 @@ describe('controllers', function() {
 
             res.body.should.eql('');
 
-            done()
-
+            Client.find().then(function(clients){
+              should(clients.length).eql(2)
+              done()
+            })
           });
       });
-
-      it('should persist the record', function(done) {
+    });
+    describe('GET /clients/{clientId}', function(){
+      let resBody = null
+      it('should retrieve a client record...', function(done) {
         request(server)
-          .get('/clients')
+          .get(`/clients/${client1._id}`)
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
           .expect(200)
           .end(function(err, res) {
             should.not.exist(err);
+            resBody = res.body
+            resBody.name.should.eql('Mark')
+            done()
+          })
+      })
+      it('...that has a normalized phone number', function(done){
+        resBody.phone.should.eql('+1 222-555-8888')
+        done()
+      })
+      it('...and a normalized DOB', function(done){
+        resBody.dob.should.eql('1980-01-01T00:00:00.000Z')
+        done()
+      })
+    })
+    describe('PUT /clients/{clientId}', function(){
+      let update = {name: 'Ashley'}
+      it('should update a client record', function(done) {
+        request(server)
+          .put(`/clients/${client1._id}`)
+          .send(update)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+            should.not.exist(err);
+            res.body.should.eql('');
 
-            should(res.body.length).be.exactly(1)
-
-            clearTestDB().then(() => {
-              done();
+            db.getClientById(client1._id).then(client => {
+              should(client.name).eql('Ashley')
+              done()
             })
-          });
-      });
-    });
+          })
+      })
+    })
+    describe('DELETE /clients/{clientId}', function(){
+      it('should delete a client record', function(done) {
+        request(server)
+          .delete(`/clients/${client1._id}`)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+            should.not.exist(err);
+            res.body.should.eql('');
+
+            db.getClients().then(clients => {
+              should(clients.length).eql(0)
+              done()
+            })
+          })
+      })
+    })
   });
 });
-
-const clearTestDB = () => {
-  return MongoClient.connect('mongodb://localhost:27017/rbc_assignment_test').then(db => {
-    return db.collection('clients').remove({})
-  })
-}
